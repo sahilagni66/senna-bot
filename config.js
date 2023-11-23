3abec8b8215f45f13e92d8c3d922a007c671a0e1
sahilagni66 <145852964+sahilagni66@users.noreply.github.com>
2023-11-23 13:45:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59172945992', 'FG98', true],
+  ['919517641203', 'FG98', true],
   ['5491168352204']
 ] //Numeros de owner 
 
 global.mods = [''] 
-global.prems = ['50489079501', '573143917092']
+global.prems = ['919517641203', '573143917092']
 global.APIs = { // API Prefix
   // name: 'https://website' 
   nrtm: 'https://fg-nrtm.ddns.net',
@@ -20,7 +20,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'Senna┃ᴮᴼᵀ' 
+global.packname = 'Felix┃ᴮᴼᵀ' 
 global.author = '@fg98' 
 
 //--info FG
